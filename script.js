251b2ec21c5ee86b9ee48297719b0515adf7cd8a
Shelby Smith <shelby.smith@poetic.io>
2019-02-27 20:25:15 -0600

    refactor

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+$(function() {
 $( "#target" ).click(function() {
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  let person;
-  let random = getRandomInt(10);
-
-  switch (random) {
-    case 0:
-        person = "Shelby";
-        break;
-    case 1:
-    case 2:
-    case 3:
-        person = "Jack";
-        break;
-    case 4:
-    case 5:
-    case 6:
-        person = "Oscar";
-        break;
-    case 7:
-    case 8:
-    case 9:
-        person ="Sarah";
-        break;
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
-    $(function() {
-    $('.namespace').text(`QA for this site is assigned to ${person}!`)
-});
+    let person;
+    let random = getRandomInt(10);
+
+    switch (random) {
+        case 0:
+            person = "Shelby";
+            break;
+        case 1:
+        case 2:
+        case 3:
+            person = "Jack";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            person = "Oscar";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            person ="Sarah";
+            break;
+        };
+        $('.namespace').text(`QA for this site is assigned to ${person}!`)
+        });
 });
